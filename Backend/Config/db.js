14cc25ff5236438aa88d2eb7c75c391d36a6966a
codakadaniel33 <kalimangasicostantino33@gmail.com>
2026-03-28 23:16:14 +0300

    preparation for deployment

--- Backend/Config/db.js
+++ Backend/Config/db.js
@@ -4,11 +4,10 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const pool = new Pool({
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
-    database: process.env.DB_NAME,
-    password: process.env.DB_PASSWORD,
-    port: process.env.DB_PORT
+    connectionString: process.env.DB_URL,
+    ssl: 
+        process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false
+    
 
 })
 
