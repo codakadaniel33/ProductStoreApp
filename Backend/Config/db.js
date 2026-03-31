0c9d0320c6c51424e51e5d714b62aefcf9c89803
codakadaniel33 <kalimangasicostantino33@gmail.com>
2026-03-31 12:07:48 +0300

    fixing supabase connection

--- Backend/Config/db.js
+++ Backend/Config/db.js
@@ -5,7 +5,11 @@ dotenv.config();
 
 const pool = new Pool({
     connectionString: process.env.DB_URL,
-    ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false
+    ssl: process.env.DB_URL?.includes('supabase.co')
+      ? { rejectUnauthorized: false }
+      : process.env.NODE_ENV === 'production'
+      ? { rejectUnauthorized: false }
+      : false
 });
 
 pool.on('error', (err) => {
